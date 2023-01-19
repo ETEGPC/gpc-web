6d6d5a519d399fb731a95e0fb826d85b5d8da20f
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-01-18 21:25:21 -0300

    testes de pwa no netlify

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ReactPWAInstallProvider enableLogging>
+
       <App />
-    </ReactPWAInstallProvider>
+
   </React.StrictMode>
 );
 
